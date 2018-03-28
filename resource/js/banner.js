82b43e3f61666d29a922562b1552f4e7c9d5d204
lpenny22 <lpenny_2002@yahoo.com>
2018-03-28 11:22:42 -0500

    Update banner.js

--- resource/js/banner.js
+++ resource/js/banner.js
@@ -1,6 +1,6 @@
 // JScript source code
 var bannertext = '<div style="margin:0 auto; width:100%;position: relative; top: 0px; left: 0px; padding-left: 0px; background-color: #000000">'
-bannertext += '\n<img src="https://raw.githubusercontent.com/dxcuni/help/itg/resource/images/dxcuhelpbanner.png" style="margin:0 auto; width:100%;height:115px;">'
+bannertext += '\n<img src="https://raw.githubusercontent.com/dxcuni/help/itg/resource/images/dxcuhelpbanner.png" style="margin:0 auto; width:100%;height:121px;">'
 bannertext += '\n</div> '
 document.write(bannertext);
 
